8da3ae94b0bfe002846a94d6f3fb47029e1fd72d
Tyler Norbury <tylernorbury96@gmail.com>
2024-02-21 18:52:34 -0800

    breakpoints and more theming

---
+++ lib/theme/breakpoints.dart
@@ -0,0 +1,13 @@
+class WidthBreakpoints {
+  /// 400
+  static const double small = 400;
+
+  /// 800
+  static const double medium = 800;
+
+  /// 1200
+  static const double large = 1200;
+
+  /// 1920
+  static const double xLarge = 1920;
+}
