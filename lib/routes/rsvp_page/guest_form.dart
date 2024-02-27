5561dca1f892534643c20cae2a82b016999ed88d
Tyler Norbury <tylernorbury96@gmail.com>
2024-02-26 18:40:39 -0800

    breakout form into on file and fix sizing

---
+++ lib/routes/rsvp_page/guest_form.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wedding_site/common/widgets/input_field.dart';
+import 'package:wedding_site/models/wedding_guest.dart';
+import 'package:wedding_site/providers/wedding_guest_provider.dart';
+import 'package:wedding_site/routes/rsvp_page/rsvp_page.dart';
+
+class GuestForm extends ConsumerWidget {
+  const GuestForm({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final guest = ref.watch(weddingGuestProvider);
+
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: DropdownButtonFormField(
+            decoration: const InputDecoration(
+              label: Text("RSVP"),
+            ),
+            value: ref.watch(rsvpProvider),
+            items: const [
+              DropdownMenuItem(
+                value: true,
+                child: Text("Yes"),
+              ),
+              DropdownMenuItem(
+                value: false,
+                child: Text("No"),
+              ),
+            ],
+            onChanged: (val) {
+              ref.read(rsvpProvider.notifier).state = val;
+              ref
+                  .read(weddingGuestProvider.notifier)
+                  .setIsComing(ref.read(rsvpProvider)!);
+            },
+          ),
+        ),
+        if (ref.watch(rsvpProvider) == true) ...[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: InputField(
+              controller: ref.watch(dietTcProvider),
+              label: "Dietary Restrictions",
+              helperText: "Optional",
+              maxLines: null,
+              onChanged: () => ref
+                  .read(weddingGuestProvider.notifier)
+                  .setDietaryRestrictions(ref.read(dietTcProvider).text),
+            ),
+          ),
+          if (guest!.plusOneAllowed) ...[
+            const SizedBox(height: 16),
+            const Divider(
+              height: 24,
+              indent: 4,
+              endIndent: 4,
+            ),
+            const PlusOneForm(),
+          ],
+        ]
+      ],
+    );
+  }
+}
+
+class PlusOneForm extends ConsumerWidget {
+  const PlusOneForm({Key? key}) : super(key: key);
+
+  void _submitPlusOneChanges(WidgetRef ref) {
+    ref.read(weddingGuestProvider.notifier).setPlusOne(
+          WeddingGuest(
+            name: ref.read(guestNameTcProvider).text,
+            dietaryInfo: ref.read(guestDietTcProvider).text,
+          ),
+        );
+  }
+
+  @override
+  Widget build(BuildContext context, ref) {
+    return Column(
+      children: [
+        SwitchListTile(
+          title: const Text("Bringing Plus-One"),
+          value: ref.watch(bringingPlusOneProvider),
+          onChanged: (val) =>
+              ref.read(bringingPlusOneProvider.notifier).state = val,
+        ),
+
+        // if bring plus one, show fields for name and dietary restrictions
+        if (ref.watch(bringingPlusOneProvider)) ...[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: InputField(
+              controller: ref.watch(guestNameTcProvider),
+              label: "Plus One's Name",
+              onChanged: () => _submitPlusOneChanges(ref),
+            ),
+          ),
+          InputField(
+            controller: ref.watch(guestDietTcProvider),
+            label: "Plus One's Dietary Restrictions",
+            helperText: "Optional",
+            maxLines: null,
+            onChanged: () => _submitPlusOneChanges(ref),
+          ),
+        ]
+      ],
+    );
+  }
+}
--- lib/routes/rsvp_page/rsvp_page.dart
+++ lib/routes/rsvp_page/rsvp_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:wedding_site/common/widgets/input_field.dart';
 import 'package:wedding_site/models/wedding_guest.dart';
 import 'package:wedding_site/providers/wedding_guest_provider.dart';
+import 'package:wedding_site/routes/rsvp_page/guest_form.dart';
 import 'package:wedding_site/theme/breakpoints.dart';
 import 'package:wedding_site/theme/theme.dart';
 
@@ -98,12 +98,12 @@ class _RsvpPageState extends ConsumerState<RsvpPage> {
     final guest = ref.watch(weddingGuestProvider);
     return LayoutBuilder(
       builder: (context, c) {
-        final width = switch (c.maxWidth) {
-          > WidthBreakpoints.large => c.maxWidth * .5,
-          < WidthBreakpoints.large => c.maxWidth * .65,
-          < WidthBreakpoints.medium => c.maxWidth * .7,
-          < WidthBreakpoints.small => c.maxWidth * .75,
-          _ => c.maxWidth - 64,
+        final padding = switch (c.maxWidth) {
+          < WidthBreakpoints.small => 32.0,
+          < WidthBreakpoints.medium => 32.0,
+          < WidthBreakpoints.large => 128.0,
+          > WidthBreakpoints.large => 256.0,
+          _ => 32.0,
         };
 
         if (guest == null) {
@@ -111,6 +111,7 @@ class _RsvpPageState extends ConsumerState<RsvpPage> {
         }
 
         return SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: padding),
           child: Column(
             children: [
               Text(
@@ -126,114 +127,6 @@ class _RsvpPageState extends ConsumerState<RsvpPage> {
           ),
         );
       },
-    );
-  }
-}
-
-class GuestForm extends ConsumerWidget {
-  const GuestForm({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final guest = ref.watch(weddingGuestProvider);
-
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: DropdownButtonFormField(
-            decoration: const InputDecoration(
-              label: Text("RSVP"),
-            ),
-            value: ref.watch(rsvpProvider),
-            items: const [
-              DropdownMenuItem(
-                value: true,
-                child: Text("Yes"),
-              ),
-              DropdownMenuItem(
-                value: false,
-                child: Text("No"),
-              ),
-            ],
-            onChanged: (val) {
-              ref.read(rsvpProvider.notifier).state = val;
-              ref
-                  .read(weddingGuestProvider.notifier)
-                  .setIsComing(ref.read(rsvpProvider)!);
-            },
-          ),
-        ),
-        if (ref.watch(rsvpProvider) == true) ...[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: InputField(
-              controller: ref.watch(dietTcProvider),
-              label: "Dietary Restrictions",
-              helperText: "Optional",
-              maxLines: null,
-              onChanged: () => ref
-                  .read(weddingGuestProvider.notifier)
-                  .setDietaryRestrictions(ref.read(dietTcProvider).text),
-            ),
-          ),
-          if (guest!.plusOneAllowed) ...[
-            const SizedBox(height: 16),
-            const Divider(
-              height: 24,
-              indent: 4,
-              endIndent: 4,
-            ),
-            const PlusOneForm(),
-          ],
-        ]
-      ],
-    );
-  }
-}
-
-class PlusOneForm extends ConsumerWidget {
-  const PlusOneForm({Key? key}) : super(key: key);
-
-  void _submitPlusOneChanges(WidgetRef ref) {
-    ref.read(weddingGuestProvider.notifier).setPlusOne(
-          WeddingGuest(
-            name: ref.read(guestNameTcProvider).text,
-            dietaryInfo: ref.read(guestDietTcProvider).text,
-          ),
-        );
-  }
-
-  @override
-  Widget build(BuildContext context, ref) {
-    return Column(
-      children: [
-        SwitchListTile(
-          title: const Text("Bringing Plus-One"),
-          value: ref.watch(bringingPlusOneProvider),
-          onChanged: (val) =>
-              ref.read(bringingPlusOneProvider.notifier).state = val,
-        ),
-
-        // if bring plus one, show fields for name and dietary restrictions
-        if (ref.watch(bringingPlusOneProvider)) ...[
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: InputField(
-              controller: ref.watch(guestNameTcProvider),
-              label: "Plus One's Name",
-              onChanged: () => _submitPlusOneChanges(ref),
-            ),
-          ),
-          InputField(
-            controller: ref.watch(guestDietTcProvider),
-            label: "Plus One's Dietary Restrictions",
-            helperText: "Optional",
-            maxLines: null,
-            onChanged: () => _submitPlusOneChanges(ref),
-          ),
-        ]
-      ],
     );
   }
 }
